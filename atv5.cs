d49d95c84544c1f06e7379f21b61db9a2e226552
EmillyMourac <emillymoura.0501@gmail.com>
2026-05-19 16:31:47 -0300

    entregando atividade FOR atualizado

--- atv5.cs
+++ atv5.cs
@@ -14,18 +14,31 @@ public static class Atv5
         Console.WriteLine("Digite um número para mostrar a tabuada:");
         int numero = int.Parse(Console.ReadLine()!);
 
-        Console.WriteLine("Usando for:");
         for (int contador = 1; contador <= 10; contador++)
         {
             Console.WriteLine($"{numero} x {contador} = {numero * contador}");
         }
+    }
+}
 
-        Console.WriteLine("\nUsando while:");
-        int contadorWhile = 1;
-        while (contadorWhile <= 10)
+public static class Atv5While
+{
+    public static void Executar()
+    {
+        Console.WriteLine("Digite um número para mostrar a tabuada:");
+        int numero = int.Parse(Console.ReadLine()!);
+
+        int contador = 1;
+
+        while (contador <= 10)
         {
-            Console.WriteLine($"{numero} x {contadorWhile} = {numero * contadorWhile}");
-            contadorWhile++;
+            Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+            contador++;
         }
     }
 }
+
+
+// A forma com for ficou mais organizada, porque ela deixa tudo em uma linha, Isso faz o código ficar mais limpo e fácil de entender.
+// O while, precisa de mais linhas e mais atenção para não esquecer o incremento.
+// Como a tabuada já tem uma quantidade fixa de repetições (de 1 a 10), o for acaba ficando mais fácil de usar.
